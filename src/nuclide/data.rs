bd8799ca7dc58b9fff6b68dd3f99865cd2bbd454
leximainw <leximainw@gmail.com>
2022-11-10 13:51:47 -0500

    feat(data): add hydrogen-7

--- src/nuclide/data.rs
+++ src/nuclide/data.rs
@@ -34,6 +34,7 @@ pub fn get_nuclides() -> Vec<Nuclide> {
         nuclide!{1, 3, (1.39e-22, 1.0e-23), Decay::neutron_emission()},
         nuclide!{1, 4, (8.6e-23, 6e-24), Decay::double_neutron_emission()},
         nuclide!{1, 5, (2.94e-22, 6.7e-23), Decay::neutron_emission()},   // NOTE: true decay mode unknown
+        nuclide!{1, 6, (6.52e-22, 5.58e-22), Decay::double_neutron_emission()},   // NOTE: true decay mode unknown
     ]
 }
 
